baa2240438a088d7e0171cb1237cf08116f6a26f
rubiksfood <rubiksfood@gmail.com>
2025-11-30 12:39:14 +0100

    feat: add auth middleware

---
+++ shopping-list/server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "CHANGE-ME"; // REMEMBER TO CHANGE THIS IN PRODUCTION!
+
+export default function auth(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorisation header missing" });
+  }
+
+  const [scheme, token] = authHeader.split(" ");
+
+  if (scheme !== "Bearer" || !token) {
+    return res.status(401).json({ message: "Invalid Authorisation header" });
+  }
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.userId = payload.userId; // string
+    return next();
+  } catch (err) {
+    console.error("Auth error:", err);
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
