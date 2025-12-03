e92a0b8694fa402ca37fd4827fea3934c1a17603
rubiksfood <rubiksfood@gmail.com>
2025-12-03 22:15:39 +0100

    refactor: moved page layout into App.jsx

---
+++ shopping-list/client/src/pages/ShoppingListPage.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar.jsx";
+import ShoppingList from "../components/ShoppingList.jsx";
+
+export default function ShoppingList() {
+  return (
+    <main className="max-w-4xl mx-auto px-4 py-6">
+      <ShoppingList />
+    </main>
+  );
+}
