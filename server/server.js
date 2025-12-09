b33d59f70f992d96fd96a1b14c74046edde8dcd2
rubiksfood <rubiksfood@gmail.com>
2025-12-09 12:25:06 +0100

    refactor: extract Express app for testing

--- server/server.js
+++ server/server.js
@@ -1,15 +1,6 @@
-import cors from "cors";
-import express from "express";
-import authRoutes from "./routes/auth.js";
-import shopItems from "./routes/shopItem.js";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5050;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authRoutes);
-app.use("/shopItem", shopItems);
 
 // start the Express server
 app.listen(PORT, () => {
